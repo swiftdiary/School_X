b2da03cba42d3b2aac5f87d40f5de1292159cfb4
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-05-18 15:49:45 +0500

    Onboardings are ready

--- School_X/Onboarding/OnboardingView.swift
+++ School_X/Onboarding/OnboardingView.swift
@@ -9,12 +9,58 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("onboarding") private var onboarding: Bool = false
+    @StateObject private var onboardingVM = OnboardingViewModel()
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.accentColor.gradient)
-                .ignoresSafeArea()
+        let onboardItem = onboardingVM.onboardItems[onboardingVM.onboardIndex]
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .fill(Color.accentColor.gradient)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image(systemName: "globe")
+                        .font(.largeTitle)
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text(onboardItem.title)
+                            .font(.largeTitle.bold())
+                        Text(onboardItem.description)
+                            .font(.headline)
+                        HStack {
+                            Spacer()
+                            Button("Next >") {
+                                withAnimation(.bouncy) {
+                                    onboardingVM.next()
+                                }
+                            }
+                            .buttonStyle(BorderedProminentButtonStyle())
+                            .font(.headline)
+                        }
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(.thinMaterial)
+                            .ignoresSafeArea()
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Skip") {
+                        onboarding = true
+                    }
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                }
+            }
+        }
+        .onChange(of: onboardingVM.onboardingEnded) { oldValue, newValue in
+            if oldValue == false, newValue == true {
+                onboarding = true
+            }
         }
     }
     
---
+++ School_X/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  OnboardingViewModel.swift
+//  School_X
+//
+//  Created by Akbar Khusanbaev on 18/05/24.
+//
+
+import Foundation
+
+final class OnboardingViewModel: ObservableObject {
+    @Published var onboardIndex: Int = 0
+    @Published var onboardingEnded: Bool = false
+    
+    // local data
+    let onboardItems: [OnboardItem] = [
+        OnboardItem(title: "Title", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", backgroundImage: nil, background3DUrl: nil),
+        OnboardItem(title: "Title2", description: "2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", backgroundImage: nil, background3DUrl: nil),
+        OnboardItem(title: "Title3", description: "3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", backgroundImage: nil, background3DUrl: nil)
+    ]
+    
+    func next() {
+        if onboardItems.count > onboardIndex + 1 {
+            onboardIndex += 1
+        } else {
+            onboardingEnded = true
+        }
+    }
+}
+
+struct OnboardItem: Identifiable {
+    let id: UUID = UUID()
+    let title: String
+    let description: String
+    let backgroundImage: String?
+    let background3DUrl: String?
+}
