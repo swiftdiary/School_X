447769a3d76b51217c4427d3f556fda74a91b533
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-05-18 14:13:28 +0500

    Initial Commit

---
+++ School_X/School_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_XApp.swift
+//  School_X
+//
+//  Created by Akbar Khusanbaev on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct School_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
