08b13b4a687be191fcf2a2d368f0b35ca771498c
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-05-18 20:28:41 +0500

    updated subjects with placeholders nad little changes to today section

--- School_X/Subjects/SubjectsView.swift
+++ School_X/Subjects/SubjectsView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct SubjectsView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            List(0..<100) { x in
+                HStack {
+                    RoundedRectangle(cornerRadius: 15.0)
+                        .fill(Color.accentColor.gradient)
+                        .frame(width: 80, height: 80)
+                    VStack(alignment: .leading) {
+                        Text("Subject Name")
+                            .font(.title2.bold())
+                        Text("Subtitle text (Optional)")
+                            .font(.caption)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                }
+            }
+            .listStyle(.plain)
+        }
+        .navigationTitle("Subjects")
     }
 }
 
 #Preview {
-    SubjectsView()
+    NavigationStack {
+        SubjectsView()
+    }
 }
--- School_X/Today/TodayView.swift
+++ School_X/Today/TodayView.swift
@@ -14,29 +14,51 @@ struct TodayView: View {
         @Bindable var navigation = navigation
         VStack {
             ScrollView {
-                RoundedRectangle(cornerRadius: 12.0)
-                    .fill(Color.orange.gradient)
+                NavigationLink(value: NavigationOption.tips) {
+                    RoundedRectangle(cornerRadius: 12.0)
+                        .fill(Color.orange.gradient)
+                        .stroke(.white.opacity(0.5),lineWidth: 3)
+                        .frame(height: 200)
+                        .overlay(content: {
+                            HStack {
+                                Text("Tips")
+                                Image(systemName: "lightbulb.min.fill")
+                            }
+                            .font(.largeTitle.bold())
+                            .foregroundStyle(.background)
+                        })
+                        .padding()
+                }
                 
-                    .stroke(.white.opacity(0.5),lineWidth: 3)
-                    .frame(height: 200)
-                    .overlay(content: {
-                        HStack {
-                            Text("Tips")
-                            Image(systemName: "lightbulb.min.fill")
-                        }
-                        .font(.largeTitle.bold())
-                        .foregroundStyle(.background)
-                    })
-                    .padding()
                 VStack(alignment: .leading) {
                     Text("Top")
                         .font(.title.bold())
                     Rectangle()
                         .frame(height: 2)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], content: {
+                            ForEach(0..<50) { x in
+                                HStack {
+                                    RoundedRectangle(cornerRadius: 15.0)
+                                        .fill(Color.accentColor.gradient)
+                                        .frame(width: 80, height: 80)
+                                    VStack(alignment: .leading) {
+                                        Text("Top Subject")
+                                            .font(.title2.bold())
+                                        Text("Subtitle to it")
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                .frame(width: 250)
+                            }
+                        })
+                    }
+                    
                 }
                 .padding()
             }
         }
+        .navigationTitle("Today")
     }
 }
 
